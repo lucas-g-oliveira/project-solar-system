ec6d1e2727541f8f83ed720b4dac874114859330
Lucas G <lucasg.devmail93@gmail.com>
2022-08-18 15:52:36 -0300

    requisito 2 -> OK

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header data-testid="my-action"></Header>
+      <main>
+        <Header data-testid="my-action"></Header>
+        <SolarSystem></SolarSystem>
+      </main>
     );
   }
 }
