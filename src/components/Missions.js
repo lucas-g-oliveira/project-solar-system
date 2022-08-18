65867a40a1cca493f1023f9741f05b3ec0355584
Lucas G <lucasg.devmail93@gmail.com>
2022-08-18 19:40:28 -0300

    Requisito 7 -> OK

---
+++ src/components/Missions.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+class Missions extends React.Component {
+
+    render(){
+
+        return (
+            <div data-testid="missions">
+
+            </div>
+        )
+    }
+}
+
+export default Missions;
